51599ff00a3034d68793152b8b084288cc31a71c
Kevin Hu <kevin.hu@purple.telstra.com>
2022-06-29 19:51:17 +1000

    Containerise discount API

---
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Net;
+using Discount.API.Entities;
+using Discount.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Discount.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class DiscountController : ControllerBase
+    {
+        private readonly IDiscountRepository _repo;
+
+        public DiscountController(IDiscountRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet("{productName}", Name = nameof(GetDiscount))]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> GetDiscount(string productName)
+        {
+            var coupon = await _repo.GetDiscount(productName);
+            return Ok(coupon);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> CreateDiscount([FromBody] Coupon coupon)
+        {
+            await _repo.CreateDiscount(coupon);
+            return CreatedAtRoute(nameof(GetDiscount), new { productName = coupon.ProductName }, coupon);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> UpdateDiscount([FromBody] Coupon coupon)
+        {
+            return Ok(await _repo.UpdateDiscount(coupon));
+        }
+
+        [HttpDelete("{productName}", Name = nameof(DeleteDiscount))]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<bool>> DeleteDiscount(string productName)
+        {
+            return Ok(await _repo.DeleteDiscount(productName));
+        }
+    }
+}
