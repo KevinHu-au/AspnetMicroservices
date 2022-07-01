1dc03abcd7390bff7dfff504d3e808f77ebb3bf6
Kevin Hu <kevin.hu@purple.telstra.com>
2022-07-01 18:45:36 +1000

    Setup the domain project, entity and value object

---
+++ src/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Ordering.Domain.Common
+{
+    public abstract class EntityBase
+    {
+        public int Id { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
