e27ebd524970ca65bc22923b95c879f42b0382ef
Kevin Hu <kevin.hu@purple.telstra.com>
2022-07-09 21:19:34 +1000

    Setup local ocelot json config file

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -3,12 +3,19 @@ using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.ConfigureAppConfiguration((hostingContext, config) => 
+{
+    config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
+});
+
 builder.Host.ConfigureLogging((hostingContext, loggingBuilder) => 
 {
     loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
     loggingBuilder.AddConsole();
     loggingBuilder.AddDebug();
 });
+
 builder.Services.AddOcelot();
 
 var app = builder.Build();
