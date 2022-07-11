fbbb7adaafea48c82be91f634554fb57249a629e
Kevin Hu <kevin.hu@purple.telstra.com>
2022-07-11 19:48:18 +1000

    Refactor order and checkout page and pagemodel

--- src/WebApps/AspnetRunBasics/Pages/CheckOut.cshtml.cs
+++ src/WebApps/AspnetRunBasics/Pages/CheckOut.cshtml.cs
@@ -1,49 +1,53 @@
-﻿using System;
-using System.Threading.Tasks;
-using AspnetRunBasics.Repositories;
+﻿using AspnetRunBasics.Models;
+using AspnetRunBasics.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AspnetRunBasics
 {
-    public class CheckOutModel : PageModel
+  public class CheckOutModel : PageModel
+  {
+    private readonly IBasketService _basketService;
+    private readonly IOrderService _orderService;
+
+    public CheckOutModel(IBasketService basketService, IOrderService orderService)
     {
-        private readonly ICartRepository _cartRepository;
-        private readonly IOrderRepository _orderRepository;
-
-        public CheckOutModel(ICartRepository cartRepository, IOrderRepository orderRepository)
-        {
-            _cartRepository = cartRepository ?? throw new ArgumentNullException(nameof(cartRepository));
-            _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
-        }
-
-        [BindProperty]
-        public Entities.Order Order { get; set; }
-
-        public Entities.Cart Cart { get; set; } = new Entities.Cart();
-
-        public async Task<IActionResult> OnGetAsync()
-        {
-            Cart = await _cartRepository.GetCartByUserName("test");
-            return Page();
-        }
-
-        public async Task<IActionResult> OnPostCheckOutAsync()
-        {
-            Cart = await _cartRepository.GetCartByUserName("test");
-
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-
-            Order.UserName = "test";
-            Order.TotalPrice = Cart.TotalPrice;
-
-            await _orderRepository.CheckOut(Order);
-            await _cartRepository.ClearCart("test");
-            
-            return RedirectToPage("Confirmation", "OrderSubmitted");
-        }       
+      _basketService = basketService ?? throw new ArgumentNullException(nameof(basketService));
+      _orderService = orderService ?? throw new ArgumentNullException(nameof(orderService));
     }
+
+    [BindProperty]
+    public BasketCheckoutModel Order { get; set; }
+
+    public BasketModel Cart { get; set; } = new BasketModel();
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+      var userName = "swn";
+      Cart = await _basketService.GetBasket(userName);
+      return Page();
+    }
+
+    public async Task<IActionResult> OnPostCheckOutAsync()
+    {
+      var userName = "swn";
+      Cart = await _basketService.GetBasket(userName);
+
+      if (!ModelState.IsValid)
+      {
+        return Page();
+      }
+
+      Order.UserName = "swn";
+      Order.TotalPrice = Cart.TotalPrice;
+
+      Order.UserName = userName;
+      Order.TotalPrice = Cart.TotalPrice;
+
+      await _basketService.CheckoutBasket(Order);
+
+      return RedirectToPage("Confirmation", "OrderSubmitted");
+
+    }
+  }
 }
--- src/WebApps/AspnetRunBasics/Pages/Order.cshtml.cs
+++ src/WebApps/AspnetRunBasics/Pages/Order.cshtml.cs
@@ -1,28 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using AspnetRunBasics.Repositories;
+﻿using AspnetRunBasics.Models;
+using AspnetRunBasics.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AspnetRunBasics
 {
-    public class OrderModel : PageModel
+  public class OrderModel : PageModel
+  {
+    private readonly IOrderService _orderService;
+
+    public OrderModel(IOrderService orderService)
     {
-        private readonly IOrderRepository _orderRepository;
-
-        public OrderModel(IOrderRepository orderRepository)
-        {
-            _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
-        }
-
-        public IEnumerable<Entities.Order> Orders { get; set; } = new List<Entities.Order>();
-
-        public async Task<IActionResult> OnGetAsync()
-        {
-            Orders = await _orderRepository.GetOrdersByUserName("test");
-
-            return Page();
-        }       
+      _orderService = orderService ?? throw new ArgumentNullException(nameof(orderService));
     }
+
+    public IEnumerable<OrderResponseModel> Orders { get; set; } = new List<OrderResponseModel>();
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+      var userName = "swn";
+      Orders = await _orderService.GetOrdersByUserName(userName);
+
+      return Page();
+    }
+  }
 }
