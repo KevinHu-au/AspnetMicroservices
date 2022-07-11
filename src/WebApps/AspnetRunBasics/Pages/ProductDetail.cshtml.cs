8e37fdee41adddac29c90c370abd6a5c6ec5841d
Kevin Hu <kevin.hu@purple.telstra.com>
2022-07-11 17:26:42 +1000

    Refactor Product detail page model

--- src/WebApps/AspnetRunBasics/Pages/ProductDetail.cshtml.cs
+++ src/WebApps/AspnetRunBasics/Pages/ProductDetail.cshtml.cs
@@ -1,52 +1,60 @@
-﻿using System;
-using System.Threading.Tasks;
-using AspnetRunBasics.Repositories;
+﻿using AspnetRunBasics.Models;
+using AspnetRunBasics.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AspnetRunBasics
 {
-    public class ProductDetailModel : PageModel
+  public class ProductDetailModel : PageModel
+  {
+    private readonly ICatalogService _catalogService;
+    private readonly IBasketService _basketService;
+
+    public ProductDetailModel(ICatalogService catalogService, IBasketService basketService)
     {
-        private readonly IProductRepository _productRepository;
-        private readonly ICartRepository _cartRepository;
-
-        public ProductDetailModel(IProductRepository productRepository, ICartRepository cartRepository)
-        {
-            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
-            _cartRepository = cartRepository ?? throw new ArgumentNullException(nameof(cartRepository));
-        }
-
-        public Entities.Product Product { get; set; }
-
-        [BindProperty]
-        public string Color { get; set; }
-
-        [BindProperty]
-        public int Quantity { get; set; }
-
-        public async Task<IActionResult> OnGetAsync(int? productId)
-        {
-            if (productId == null)
-            {
-                return NotFound();
-            }
-
-            Product = await _productRepository.GetProductById(productId.Value);
-            if (Product == null)
-            {
-                return NotFound();
-            }
-            return Page();
-        }
-
-        public async Task<IActionResult> OnPostAddToCartAsync(int productId)
-        {
-            //if (!User.Identity.IsAuthenticated)
-            //    return RedirectToPage("./Account/Login", new { area = "Identity" });
-
-            await _cartRepository.AddItem("test", productId, Quantity, Color);
-            return RedirectToPage("Cart");
-        }
+      _catalogService = catalogService ?? throw new ArgumentNullException(nameof(catalogService));
+      _basketService = basketService ?? throw new ArgumentNullException(nameof(basketService));
     }
+
+    public CatalogModel Product { get; set; }
+
+    [BindProperty]
+    public string Color { get; set; }
+
+    [BindProperty]
+    public int Quantity { get; set; }
+
+    public async Task<IActionResult> OnGetAsync(string productId)
+    {
+      if (productId == null)
+      {
+        return NotFound();
+      }
+
+      Product = await _catalogService.GetCatalog(productId);
+      if (Product == null)
+      {
+        return NotFound();
+      }
+      return Page();
+    }
+
+    public async Task<IActionResult> OnPostAddToCartAsync(string productId)
+    {
+      var product = await _catalogService.GetCatalog(productId);
+      var userName = "swn";
+      var basket = await _basketService.GetBasket(userName);
+
+      basket.Items.Add(new BasketItemModel
+      {
+        ProductId = productId,
+        ProductName = product.Name,
+        Price = product.Price,
+        Quantity = 1,
+        Color = "Black"
+      });
+      await _basketService.UpdateBasket(basket);
+      return RedirectToPage("Cart");
+    }
+  }
 }
