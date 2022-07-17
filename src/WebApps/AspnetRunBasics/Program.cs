1fc3e3417f0345867da47e4f24a9a6a055ead3eb
Kevin Hu <kevin.hu@purple.telstra.com>
2022-07-17 18:21:34 +1000

    Config logging for Webapp

--- src/WebApps/AspnetRunBasics/Program.cs
+++ src/WebApps/AspnetRunBasics/Program.cs
@@ -33,7 +33,7 @@ else
   app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
--- src/WebApps/AspnetRunBasics/Services/CatalogService.cs
+++ src/WebApps/AspnetRunBasics/Services/CatalogService.cs
@@ -10,15 +10,18 @@ namespace AspnetRunBasics.Services
 {
     public class CatalogService : ICatalogService
     {
-        private readonly HttpClient _client;        
+        private readonly HttpClient _client;
+        private readonly ILogger<CatalogService> _logger;
 
         public CatalogService(HttpClient client, ILogger<CatalogService> logger)
         {
             _client = client ?? throw new ArgumentNullException(nameof(client));
+            _logger = logger;
         }
 
         public async Task<IEnumerable<CatalogModel>> GetCatalog()
         {
+            _logger.LogDebug("Getting catalog products from url: {url}", _client.BaseAddress);
             var response = await _client.GetAsync("/Catalog");
             return await response.ReadContentAs<List<CatalogModel>>();
         }
