ffc82d64060044591bac727aa5fc742f5cd801cc
Kevin Hu <kevin.hu@purple.telstra.com>
2022-07-18 21:13:07 +1000

    Add logging to Discount API

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,10 +1,12 @@
+using Common.Logging;
 using Discount.API.Extensions;
 using Discount.API.Repositories;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog(SeriLogger.Configure);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
