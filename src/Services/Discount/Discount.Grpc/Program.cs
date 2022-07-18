0eab7292a635cc7b2fa4119c13abf95e0766f83b
Kevin Hu <kevin.hu@purple.telstra.com>
2022-07-18 21:18:57 +1000

    Add logging to Discount Grpc service

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,10 +1,13 @@
+using System.Security.AccessControl;
 // using Discount.Grpc.Services;
 using Discount.Grpc.Repositories;
 using Discount.Grpc.Extensions;
 using Discount.Grpc.Services;
+using Serilog;
+using Common.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Host.UseSerilog(SeriLogger.Configure);
 // Additional configuration is required to successfully run gRPC on macOS.
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
