c273fa3cf489bf454f69f9379e1c3103374a9432
Kevin Hu <kevin.hu@purple.telstra.com>
2022-07-19 21:39:05 +1000

    Add health check to web app (to the Ocelog api gw)

--- src/WebApps/AspnetRunBasics/Program.cs
+++ src/WebApps/AspnetRunBasics/Program.cs
@@ -1,5 +1,8 @@
 using AspnetRunBasics.Services;
 using Common.Logging;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Serilog;
 using WebApps.AspnetRunBasics.Extensions;
 
@@ -26,6 +29,9 @@ builder.Services.AddHttpClient<IOrderService, OrderService>(c => c.BaseAddress =
 
 builder.Services.AddRazorPages();
 
+builder.Services.AddHealthChecks()
+                .AddUrlGroup(new Uri($"{builder.Configuration["ApiSettings:GatewayAddress"]}"), "Ocelog API Gateway", HealthStatus.Degraded);
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -50,4 +56,11 @@ app.UseEndpoints(endpoints =>
 {
     endpoints.MapRazorPages();
 });
+
+app.MapHealthChecks("/hc", new HealthCheckOptions()
+                            {
+                                Predicate = _ => true,
+                                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                            });
+
 app.Run();
